792479777f3f835a802f9e39b122afe8c8d421f1
Hafis Ali <hafisali007@gmail.com>
2021-06-21 21:32:43 +0400

    Project Initialization

---
+++ ContactManagement/ContactManagement/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManagement.Models
+{
+    public class Contact
+    {
+        public long? id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public status status { get; set; }
+    }
+
+    public enum status {
+        Active, Inactive
+    }
+
+}
---
+++ ContactManagement/ContactManagement/Models/ContactAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagement.Models
+{
+    public class ContactAppContext:DbContext
+    {
+        public ContactAppContext(DbContextOptions<ContactAppContext> options)
+              : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+    }
+}
