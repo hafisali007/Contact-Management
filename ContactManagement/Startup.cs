4c527bd5fdf29280e8e23f0c36ef9d97c60356dc
Hafis Ali <hafisali007@gmail.com>
2021-06-24 12:05:02 +0400

    Up

--- ContactManagement/Startup.cs
+++ ContactManagement/Startup.cs
@@ -45,7 +45,8 @@ namespace ContactManagement
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
             }
---
+++ ContactManagement/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/ContactManagement.Models.ContactAppContext.sql
@@ -0,0 +1,89 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210622072747_InitialAzure')
+BEGIN
+    CREATE TABLE [Contacts] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(150) NULL,
+        [LastName] nvarchar(100) NULL,
+        [Email] nvarchar(150) NULL,
+        [PhoneNumber] nvarchar(15) NULL,
+        [Status] int NOT NULL,
+        CONSTRAINT [PK_Contacts] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210622072747_InitialAzure')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210622072747_InitialAzure', N'5.0.7');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210622114403_Initial')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Contacts]') AND [c].[name] = N'PhoneNumber');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Contacts] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Contacts] ALTER COLUMN [PhoneNumber] nvarchar(15) NOT NULL;
+    ALTER TABLE [Contacts] ADD DEFAULT N'' FOR [PhoneNumber];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210622114403_Initial')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Contacts]') AND [c].[name] = N'FirstName');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Contacts] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Contacts] ALTER COLUMN [FirstName] nvarchar(150) NOT NULL;
+    ALTER TABLE [Contacts] ADD DEFAULT N'' FOR [FirstName];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210622114403_Initial')
+BEGIN
+    DECLARE @var2 sysname;
+    SELECT @var2 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Contacts]') AND [c].[name] = N'Email');
+    IF @var2 IS NOT NULL EXEC(N'ALTER TABLE [Contacts] DROP CONSTRAINT [' + @var2 + '];');
+    ALTER TABLE [Contacts] ALTER COLUMN [Email] nvarchar(150) NOT NULL;
+    ALTER TABLE [Contacts] ADD DEFAULT N'' FOR [Email];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210622114403_Initial')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210622114403_Initial', N'5.0.7');
+END;
+GO
+
+COMMIT;
+GO
+
