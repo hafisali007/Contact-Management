325221037edf01a58d827763a50840ed01715a76
Hafis Ali <hafisali007@gmail.com>
2021-07-26 10:09:18 +0400

    Test

--- ContactManagement/Controllers/Api/ContactApiController.cs
+++ ContactManagement/Controllers/Api/ContactApiController.cs
@@ -12,8 +12,7 @@ namespace ContactManagement.Controllers.Api
     [ApiController]
     public class ContactApiController : ControllerBase
     {
-
-        private readonly ContactAppContext _context;
+                private readonly ContactAppContext _context;
 
         // initiate database context
         public ContactApiController(ContactAppContext context)
